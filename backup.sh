6e2d1df2ef53d8beeddb413818e3911869f10e83
danic85 <daniel.nicholson@sage.com>
2015-11-28 21:56:28 +0000

    Reconnect wifi if dropped

--- backup.sh
+++ backup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 source config.cfg
 
+# Connect to wifi if connection dropped
+if ifconfig wlan0 | grep -q "inet addr:" ; then
+	#do nothing
+	echo "internet connected"
+else
+	ifup --force wlan0
+fi
+
+# Mount drive if not mounted
 sudo mount /mnt/$drive_name > /dev/null 2>&1
 
+# Copy all new or changed files
 cp -r -u /mnt/$drive_name/* $backup_path/
