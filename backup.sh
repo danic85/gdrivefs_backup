84cfcf3455e1ab31353c2e452330fff20c0df0b0
danic85 <daniel.nicholson@sage.com>
2015-11-28 21:46:04 +0000

    Quiet mount drive

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source config.cfg
 
-sudo mount $drive_name
+sudo mount /mnt/$drive_name > /dev/null 2>&1
 
-cp -r -u /mnt/$drive_name $backup_path
+cp -r -u /mnt/$drive_name/* $backup_path/
